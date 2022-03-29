cd28b047ae6a1034eaa42fd40c3b3fe19f993afa
Erick Lozano <erickklozano3@gmail.com>
2022-03-29 14:28:28 -0500

    Create new customer form v1 #4

--- src/app/features/users/components/user-home/user-home.component.ts
+++ src/app/features/users/components/user-home/user-home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-home',
@@ -7,16 +8,13 @@ import { UsersService } from '../../services/users.service';
   styleUrls: ['./user-home.component.scss']
 })
 export class UserHomeComponent {
-
   newItem: any[] = [];
   cardView = true;
   tableView = false;
+  addCustomerForm = false;
 
 
-
-  constructor(public homeData: UsersService) {
-
-  }
+  constructor(public homeData: UsersService) { }
 
   toggleCardView() {
     this.cardView = true;
@@ -28,15 +26,21 @@ export class UserHomeComponent {
     this.tableView = true;
   }
 
+  toggleAddCustomerForm() {
+    if (this.addCustomerForm == false) {
+      this.addCustomerForm = true;
+    }
+    else {
+      this.addCustomerForm = false;
+    }
+  }
 
   getIconCheck(gender: string) {
-    if (gender == 'male') {
+    if (gender.toLowerCase() == 'male') {
       return '../../../../../assets/maleIcon.png'
     }
     return '../../../../../assets/femaleIcon.png'
   }
-
-
 
 }
 
--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UserHomeComponent } from './components/user-home/user-home.component';
 import { UserOrderComponent } from './components/user-order/user-order.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserHomeComponent, UserOrderComponent],
   imports: [
-    CommonModule, NgxPaginationModule
+    CommonModule, NgxPaginationModule, FormsModule
   ],
   exports: [
     UserHomeComponent, UserOrderComponent
